472d6ca3fa47e7d146e0ada0966b00a709624413
realwildgeek <realwildgeek@gmail.com>
2026-08-30 17:58:08 +0800

    Update vue.config.js

--- web/vue.config.js
+++ web/vue.config.js
@@ -5,7 +5,7 @@ const isLibrary = process.env.NODE_ENV === 'library'
 const WebpackDynamicPublicPathPlugin = require('webpack-dynamic-public-path')
 
 module.exports = {
-  publicPath: isDev ? '' : './dist',
+  publicPath: './', // 把原来的内容替换成 './'
   outputDir: '../dist',
   lintOnSave: false,
   productionSourceMap: false,
